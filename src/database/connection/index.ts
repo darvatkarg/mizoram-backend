036d107e76600222fe6ffac208e545809adb03ea
prathmeshpote99 <poteprathmesh@gmail.com>
2023-11-10 17:18:51 +0530

    first commit

---
+++ src/database/connection/index.ts
@@ -0,0 +1,17 @@
+import config from "config";
+import mongoose from "mongoose";
+import express from "express";
+const mongooseConnection = express();
+const dbUrl: any = config.get("db_url");
+console.log(dbUrl);
+
+mongoose
+  .connect(dbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then((result) => console.log("Database successfully connected"))
+  .catch((err) => console.log(err));
+export { mongooseConnection };
